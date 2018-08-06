55a8d9a8afaf8bf2241a3a004d9132fe3a690487
yeshihao <706722153@qq.com>
2018-08-06 21:57:38 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 // import router from './router'
+import fastclick from 'fastclick'
 
 import 'common/stylus/index.styl'
+
+fastclick.attach(document.body)
+// 去除300ms的延迟
 
 /* eslint-disable no-new */
 new Vue({
